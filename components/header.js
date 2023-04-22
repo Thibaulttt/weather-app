97e10fe7aa49102418355ad085f1884cc7e9d05f
Thibault <thibault.le.guillou79@gmail.com>
2023-04-22 19:47:18 +0200

    Edit icons and README

--- components/header.js
+++ components/header.js
@@ -1,14 +1,17 @@
 import Image from 'next/image';
 import styles from "../styles/Landing.module.css";
+import moment from "moment";
+import { dayFormat, times } from "../utils/dayTimes";
 
 const Header = ({ name, country, localDayTime, iconLabel }) => {
     // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
     function getIcon(iconLabel) {
-        console.log(iconLabel)
+        console.info(iconLabel, moment(localDayTime, dayFormat))
         let iconName = '';
+        let sunIsUp = moment(localDayTime, dayFormat).isBetween(times.dawnBeginning, times.twilightBeginning)
         switch (iconLabel) {
             case "clear sky":
-                iconName = '/clear-day.svg';
+                iconName = sunIsUp ? '/clear-day.svg' : '/night.svg';
                 break;
             case "few clouds":
                 iconName = '/few-clouds-day.svg';
